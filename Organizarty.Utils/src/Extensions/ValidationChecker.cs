6de6388de484ecd5923f5544f8f725e79dbda783
lu-css <dossantossilvalucas963@gmail.com>
2023-12-31 16:51:53 -0300

    [feat] Validation helper with fluentValidation

---
+++ Organizarty.Utils/src/Extensions/ValidationChecker.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+using Organizarty.Domain.Exceptions;
+
+namespace Organizarty.Utils.Extensions;
+
+public static class ValidatorExtension
+{
+    public static void Check<T>(this IValidator<T> validator, T value, string message)
+    {
+        var result = validator.Validate(value);
+
+        if (result.IsValid)
+        {
+            return;
+        }
+
+        var errors = result
+          .Errors
+          .Select(e => new ValidationError(e.PropertyName, e.ErrorMessage))
+          .ToList();
+
+        throw new ValidationFailException(message, errors);
+    }
+}
